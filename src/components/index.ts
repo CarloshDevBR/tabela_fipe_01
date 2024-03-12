074f5a96e09c12740e4ada498e82069f2c55be7c
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-03-12 17:33:48 -0300

    chore: components exportados

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Paragraph } from './paragraph';
+import { Dropdown } from './dropdown';
+import { Button } from './button';
+import { Autocomplete } from './autocomplete';
+
+export { Paragraph, Dropdown, Button, Autocomplete };
