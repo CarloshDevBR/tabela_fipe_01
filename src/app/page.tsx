93b8cdee8c92238d7d1d277f560cf734d8494749
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-03-13 18:00:37 -0300

    refactor: dropdown alterado para autocomplete, e button alterado para buttonLink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,90 +2,120 @@
 
 import { useEffect } from 'react';
 
-import { MenuItem, SelectChangeEvent } from '@mui/material';
+import { MenuItem, SelectChangeEvent, TextField } from '@mui/material';
 
-import { Button, Dropdown, Paragraph } from '@/components';
+import { Autocomplete, ButtonLink, Dropdown, Paragraph, RenderIf } from '@/components';
 
 import { useGetAllSequentialDataFipe } from '@/hook/useGetAllSequentialDataFipe';
+
 import { ACTIONS, useFormFipe } from '@/hook/useFormFipe';
 
+import { Fipe } from '@/types';
+
 export default function HomeForm() {
-  const { state, dispatch } = useFormFipe();
+  const { state, completed, dispatch } = useFormFipe();
 
-  const { brands, models, years, getBrands, getMOdels, getYears } = useGetAllSequentialDataFipe();
+  const {
+    dataBrands,
+    isLoadingBrands,
+    dataModels,
+    isLoadingModels,
+    dataYears,
+    isLoadingYears,
+    getBrands,
+    getModels,
+    getYears,
+  } = useGetAllSequentialDataFipe();
 
-  useEffect(() => {
-    if (!brands) {
-      getBrands();
+  const handleSelectBrand = (_: React.SyntheticEvent<Element, Event>, value: Fipe | null) => {
+    dispatch({ type: ACTIONS.SET_BRAND, payload: value?.codigo || '' });
+
+    if (state.model || state.year) {
+      dispatch({ type: ACTIONS.SET_MODEL, payload: '' });
+
+      dispatch({ type: ACTIONS.SET_YEAR, payload: '' });
     }
-  }, []);
 
-  const handleSelectBrand = (value: SelectChangeEvent<unknown>) => {
-    const id = value.target.value as string;
-
-    dispatch({ type: ACTIONS.SET_BRAND, payload: id });
-
-    getMOdels(id);
+    if (value) return getModels(value.codigo);
   };
 
-  const handleSelectModels = (value: SelectChangeEvent<unknown>) => {
-    const id = value.target.value as string;
+  const handleSelectModels = (_: React.SyntheticEvent<Element, Event>, value: Fipe | null) => {
+    dispatch({ type: ACTIONS.SET_MODEL, payload: value?.codigo || '' });
 
-    dispatch({ type: ACTIONS.SET_MODEL, payload: value.target.value as string });
+    if (state.year) {
+      dispatch({ type: ACTIONS.SET_YEAR, payload: '' });
+    }
 
-    getYears(state.model, id);
+    if (value) return getYears(state.brand, value.codigo);
   };
 
   const handleSelectYears = (value: SelectChangeEvent<unknown>) => {
     dispatch({ type: ACTIONS.SET_YEAR, payload: value.target.value as string });
   };
 
+  const handleNavigationDetails = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (!completed) return e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!dataBrands) {
+      getBrands();
+    }
+  }, []);
+
   return (
-    <main className="h-screen flex items-center justify-center bg-violet-50">
+    <main className="h-full flex items-center justify-center bg-violet-50">
       <section className="flex flex-col gap-4 min-w-[250px] max-w-[500px] w-full">
         <div className="flex flex-col">
           <Paragraph className="text-3xl text-gray-800 text-center mb-2">Tabela Fipe</Paragraph>
 
-          <Paragraph className="text-xl text-gray-800 text-center">Consulte o valor de um veículo de forma gratuita</Paragraph>
+          <Paragraph className="text-xl text-gray-800 text-center">
+            Consulte o valor de um veículo de forma gratuita
+          </Paragraph>
         </div>
 
         <form className="flex flex-col items-center gap-4 py-8 px-12 bg-white rounded-lg shadow-md">
-          <Dropdown
-            label="Marca"
-            data={brands}
-            renderMenuItem={(item) => (
-              <MenuItem key={item.codigo} value={item.codigo}>
-                {item.nome}
-              </MenuItem>
-            )}
+          <Autocomplete
+            options={dataBrands || []}
+            getOptionLabel={(item) => item.nome}
             onChange={handleSelectBrand}
+            renderInput={(params) => <TextField {...params} label="Marca" />}
+            loading={isLoadingBrands}
+            disabled={isLoadingBrands}
           />
 
-          <Dropdown
-            label="Modelo"
-            data={models}
-            renderMenuItem={(item) => (
-              <MenuItem key={item.codigo} value={item.codigo}>
-                {item.nome}
-              </MenuItem>
-            )}
+          <Autocomplete
+            options={dataModels || []}
+            getOptionLabel={(item) => item.nome}
             onChange={handleSelectModels}
+            renderInput={(params) => <TextField {...params} label="Modelo" />}
+            loading={isLoadingModels}
+            disabled={!state.brand}
           />
 
-          <Dropdown
-            label="Ano"
-            data={years}
-            renderMenuItem={(item) => (
-              <MenuItem key={item.codigo} value={item.codigo}>
-                {item.nome}
-              </MenuItem>
-            )}
-            onChange={handleSelectYears}
-          />
+          <RenderIf conditional={Boolean(state.brand && state.model)}>
+            <Dropdown
+              label="Ano"
+              data={dataYears || []}
+              renderMenuItem={(item) => (
+                <MenuItem key={item.codigo} value={item.codigo}>
+                  {item.nome}
+                </MenuItem>
+              )}
+              onChange={handleSelectYears}
+              disabled={!state.model}
+              variant="outlined"
+              isLoading={isLoadingYears}
+            />
+          </RenderIf>
 
-          <Button className="w-44 h-10 mt-4 bg-violet-800 normal-case hover:bg-purple-700" variant="contained" disabled={true}>
+          <ButtonLink
+            href={`/detalhes/?brand=${state.brand}&model=${state.model}&year=${state.year}`}
+            onClick={handleNavigationDetails}
+            disabled={!completed}
+          >
             Consultar preço
-          </Button>
+          </ButtonLink>
         </form>
       </section>
     </main>
