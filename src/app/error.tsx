a286e7c252d9e2d257a55307439f5224943ebb44
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-03-13 17:51:05 -0300

    feat: error boundary criado

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { Button } from '@/components';
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="w-full h-full bg-violet-50 flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-6 p-10 bg-white rounded-lg shadow border">
+        <h2 className="text-red-primary-companny">Ocorreu um erro desconhecido nessa página!</h2>
+
+        <Button className="text-[0.75rem] normal-case" variant="outlined" onClick={() => reset()}>
+          Tentar novamente
+        </Button>
+      </div>
+    </div>
+  );
+}
