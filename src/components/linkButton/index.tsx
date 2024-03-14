87b95b44ec2d9682071836fe3b623cbd44942b15
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-03-14 09:13:47 -0300

    style: cor de texto e cursor alterados quando disabilitado

--- src/components/linkButton/index.tsx
+++ src/components/linkButton/index.tsx
@@ -10,7 +10,7 @@ export const ButtonLink = ({ children, disabled, buttonClass, ...rest }: ButtonL
   return (
     <Link
       data-disabled={disabled}
-      className={`px-9 py-3 mt-4 bg-violet-800 hover:bg-purple-700 active:opacity-70 rounded-md shadow text-white data-[disabled=true]:opacity-25 data-[disabled=true]:bg-gray-500 ${buttonClass}`}
+      className={`px-9 py-3 mt-4 bg-violet-800 hover:bg-purple-700 active:opacity-70 rounded-md shadow text-white data-[disabled=true]:opacity-25 data-[disabled=true]:bg-gray-500 data-[disabled=true]:text-gray-800  data-[disabled=true]:cursor-not-allowed ${buttonClass}`}
       {...rest}
     >
       {children}
