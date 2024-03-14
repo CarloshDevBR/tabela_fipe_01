210c11e2b6f00b189fcd83833a58741e195c5a12
CarlosHDevBR <carlos.sortudox1@gmail.com>
2024-03-13 21:51:11 -0300

    feat: loading adicionado e dados em tela

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,24 +1,47 @@
 'use client';
 
+import { Loading, Paragraph } from '@/components';
+
 import { useGetParams } from '@/hook/useGetParams';
 
 import { FETCH } from '@/services/fetch';
 
 import { DetailsFipe } from '@/types';
 
-import { useMutation } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Details() {
   const { brand, model, year } = useGetParams();
 
-  const { data } = useMutation<DetailsFipe>({
-    mutationKey: [brand, model, year],
-    mutationFn: () =>
+  const { data, isPending } = useQuery<DetailsFipe>({
+    queryKey: [brand, model, year],
+    queryFn: () =>
       FETCH({
-        url: `/marcas/${brand}/modelos/${model}/anos/${year}`,
+        url: `marcas/${brand}/modelos/${model}/anos/${year}`,
         method: 'GET',
       }),
   });
 
-  return <div>dasd</div>;
+  if (isPending)
+    return (
+      <div className="h-full flex items-center justify-center bg-teal-100">
+        <Loading color="success" size={50} />
+      </div>
+    );
+
+  return (
+    <div className="h-full flex items-center justify-center bg-teal-100">
+      <section className="max-w-[600px] flex flex-col items-center gap-4">
+        <Paragraph className="text-xl text-center text-gray-700">
+          Tabela Fipe: Preço {data?.Marca} {data?.Modelo} {data?.AnoModelo}
+        </Paragraph>
+
+        <div className="px-3 py-[6px] bg-emerald-600 rounded-3xl">
+          <Paragraph className="text-white">{data?.Valor}</Paragraph>
+        </div>
+
+        <Paragraph className="text-xs text-gray-500">Este é o preço de compra do veículo</Paragraph>
+      </section>
+    </div>
+  );
 }
