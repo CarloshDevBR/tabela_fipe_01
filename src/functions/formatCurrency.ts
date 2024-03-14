c24a2a72b0ff9ec25b52cc602cba5801682f859c
CarlosHDevBR <carlos.sortudox1@gmail.com>
2024-03-13 21:54:22 -0300

    deleted: funcao formatCurrency deletada

--- src/functions/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (value: number): string => {
-  const formatted: string = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value);
-
-  return formatted;
-};
